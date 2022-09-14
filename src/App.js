2ec04fc1cd179e00f8011ded5a0575d695d6a290
Korneliu <olbiton@gmail.com>
2022-09-13 19:10:39 -0700

    added Route for task

--- src/App.js
+++ src/App.js
@@ -3,16 +3,16 @@ import './App.css';
 import {Routes,Route} from 'react-router-dom'
 import Home from '../src/pages/Home/Home'
 import Layout from './components/Layout/Layout';
+import Tasks from './pages/Tasks/Tasks';
 
 function App() {
   return (
     <Routes>
       <Route path="/" element={<Layout />}>
-
-
+        <Route index element = {<Home />} />
+        <Route path="/tasks" element={<Tasks />} />
       </Route>
     </Routes>
-
   );
 }
 
--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -2,13 +2,13 @@
 import { Outlet } from 'react-router-dom';
 
 const Layout = () => {
-    return (
-        <>
-            <main>
-                <Outlet />
-            </main>
-        </>
-    )
+  return (
+    <>
+      <main>
+        <Outlet />
+      </main>
+    </>
+  )
 }
 
 export default Layout
