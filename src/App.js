bd5d3bd75e3fb926c7bd90dda4f391f3ef1829f7
Korneliu <olbiton@gmail.com>
2022-09-13 19:44:08 -0700

    added Route index element Home

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ function App() {
   return (
     <Routes>
       <Route path="/" element={<Layout />}>
-
+        <Route index element = {<Home />} />
         <Route path="/tasks" element={<Tasks />} />
       </Route>
     </Routes>
