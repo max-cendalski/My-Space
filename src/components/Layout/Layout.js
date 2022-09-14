4270b8d415ab479033cfb9bfff9d96c881c4a256
Korneliu <olbiton@gmail.com>
2022-09-13 19:38:18 -0700

    added empty Navbar component

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -1,9 +1,11 @@
-
+import  Navbar  from '../Navbar/Navbar';
 import { Outlet } from 'react-router-dom';
+
 
 const Layout = () => {
   return (
     <>
+      <Navbar />
       <main>
         <Outlet />
       </main>
