4846113d91220a7d517c2717c437a5a6826e5f8b
Korneliu <olbiton@gmail.com>
2022-09-13 18:39:19 -0700

    started AuthContext component

---
+++ src/context/AuthContext.js
@@ -0,0 +1,9 @@
+import { useContext, createContext, useEffect, useState } from "react";
+
+const AuthContext = createContext()
+
+export const AuthContextProvider = ({children}) => {
+
+}
+
+export default AuthContext
