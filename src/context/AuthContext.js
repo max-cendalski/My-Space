45bdb49134074239f50508f847bed9dca06b546e
Korneliu <olbiton@gmail.com>
2022-09-15 14:54:34 -0700

    initial commit

--- src/context/AuthContext.js
+++ src/context/AuthContext.js
@@ -1,4 +1,5 @@
 import { useContext, createContext, useEffect, useState } from "react";
+import {GoogleAuthProvider, signInWithPopup, signInWithRedirect, signOut, onAuthStateChanged} from 'firebase/auth';
 
 const AuthContext = createContext()
 
