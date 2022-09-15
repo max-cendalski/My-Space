0136296a09adc67b5f4b014157a6df349b525b56
Korneliu <olbiton@gmail.com>
2022-09-15 15:30:55 -0700

    added RequireAuth component

---
+++ src/RequireAuth/RequireAuth.js
@@ -0,0 +1,14 @@
+import { useLocation, Navigate, Outlet } from 'react-router-dom';
+import { UserAuth } from '../context/AuthContext'
+
+const RequireAuth = () => {
+  const {user} = UserAuth()
+  const location = useLocation()
+  return (
+    (user)
+      ? <Outlet />
+      : <Navigate to="/signIn" state={{from: location}} replace />
+  );
+}
+
+export default RequireAuth;
