5e2b5f83dc51c4a137abf64c6b101e6ee3520eb5
Korneliu <olbiton@gmail.com>
2022-09-15 17:47:04 -0700

    fixed .env

--- src/components/SignIn/SignIn.js
+++ src/components/SignIn/SignIn.js
@@ -1,21 +1,20 @@
 import React from 'react';
-import { useEffect, useState } from 'react';
+import { useEffect } from 'react';
 import { UserAuth } from '../../context/AuthContext'
 import { useNavigate, useLocation } from 'react-router-dom';
 
 const SignIn = () => {
   const {googleSignIn, user} = UserAuth()
-  const [loc, setLoc] = useState('')
 
   const navigate = useNavigate()
   const location = useLocation()
   const from = location.state?.from?.pathname || "/";
 
-  useEffect(() => {
+/*   useEffect(() => {
     if (user != null) {
       navigate(from)
     }
-  },[user])
+  },[user]) */
 
   const handleGoogleSignIn = async () => {
     try {
--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -1,8 +1,10 @@
 import { NavLink } from 'react-router-dom';
+import SignIn from '../../components/SignIn/SignIn';
 
 const Home = () => {
   return (
     <article className="home-container">
+    <SignIn />
       <NavLink className="feature-button" to ="/notes" >Notes</NavLink>
       <NavLink className="feature-button" to ="/tasks" >Tasks</NavLink>
       <NavLink className="feature-button" to ="/weather" >Weather</NavLink>
