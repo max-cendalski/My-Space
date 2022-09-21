05400f6d3d00c76d53c8b46d5c0268ba5cedc8c9
Korneliu <olbiton@gmail.com>
2022-09-20 19:54:59 -0700

    added goHome button to Navbar

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,10 +1,12 @@
 import { UserAuth } from "../../context/AuthContext"
 import { Link } from "react-router-dom"
+import { useNavigate } from "react-router-dom"
 
 
 
 const Navbar = () => {
   const {user, logOut} = UserAuth()
+  const navigate = useNavigate()
 
    const handleSignOut = async () => {
     try {
@@ -14,12 +16,19 @@ const Navbar = () => {
     }
   }
 
+  const goHome = () => {
+    navigate('/')
+  }
+
   return (
 
     <article className="navbar-container">
       {
         (user) ?
-        <button onClick={handleSignOut}>Logout</button>
+        <article>
+          <button onClick={goHome}>Home</button>
+          <button onClick={handleSignOut}>Logout</button>
+        </article>
         :
         <Link className="signin-link" to ="/login">SignIn</Link>
       }
