94e7652292b480b69537211a96345f1d84026583
Korneliu <olbiton@gmail.com>
2022-09-28 16:47:05 -0700

    added Ideas component

---
+++ src/pages/Ideas/Ideas.js
@@ -0,0 +1,14 @@
+import Navbar from "../../components/Navbar/Navbar";
+
+const Ideas = () => {
+  return (
+    <>
+      <Navbar />
+      <article id="ideas-page-container">
+        <h1>Ideas</h1>
+      </article>
+    </>
+  );
+};
+
+export default Ideas;
