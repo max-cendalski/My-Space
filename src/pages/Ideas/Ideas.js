0af48c6b50b471175990b3ce4553cf9d5f846bc9
Korneliu <olbiton@gmail.com>
2022-09-28 20:16:17 -0700

    created idea generator

--- src/pages/Ideas/Ideas.js
+++ src/pages/Ideas/Ideas.js
@@ -1,31 +1,48 @@
 import Navbar from "../../components/Navbar/Navbar";
 import GoBack from "../../components/GoBack/GoBack";
-import { getDocs,getDoc, doc,collection } from "firebase/firestore";
+import { getDocs, getDoc, doc, collection } from "firebase/firestore";
 import { db } from "../../firebase/Firebase";
 import { useState, useEffect } from "react";
 
 const Ideas = () => {
   const [ideas, setIdeas] = useState([]);
-  const [singleIdea, setSingleIdea] = useState('')
-  const [ideaToRender, setIdeaToRender] = useState('')
+  const [singleIdea, setSingleIdea] = useState("");
+  const [ideasToRender, setIdeasToRender] = useState("");
 
   useEffect(() => {
     const fetchIdeas = async () => {
       const singleIdeaRef = doc(db, "ideas", "N5Kp9pe6M9mFbRrd8MZq");
       try {
         const ideasData = await getDocs(collection(db, "ideas"));
-        const singleIdeaData = await getDoc(singleIdeaRef)
-        setSingleIdea(singleIdeaData.data())
-        console.log("singleIdeaData", singleIdea);
+        const singleIdeaData = await getDoc(singleIdeaRef);
+        //setSingleIdea(singleIdeaData.data())
+        //console.log("singleIdeaData", singleIdea);
         setIdeas(ideasData.docs.map((doc) => ({ ...doc.data(), id: doc.id })));
       } catch (err) {
         console.error("ERROR", err);
       }
     };
-    fetchIdeas()
+    fetchIdeas();
     // eslint-disable-next-line
   }, []);
-    //console.log("ideas", ideas);
+
+  const handleGenerateIdeas = () => {
+    let numbers = [];
+    let counter = 0;
+    let number = 0;
+    for (var i = 0; i < 3; i++) {
+      number = Math.floor(Math.random() * ideas.length);
+      if (!numbers.includes(number)) {
+        numbers.push(ideas[number]);
+        counter++;
+      } else if (numbers.includes(number)) {
+        number = Math.floor(Math.random() * ideas.length);
+        numbers.push(ideas[number]);
+        counter++;
+      }
+    }
+    setIdeasToRender(numbers);
+  };
 
   return (
     <>
@@ -33,9 +50,16 @@ const Ideas = () => {
       <article id="ideas-page-container">
         <GoBack />
         <h1>Ideas</h1>
-        <button className="generate-ideas-button">Generate 3 ideas</button>
-
-        <p>{singleIdea.text}</p>
+        <button onClick={handleGenerateIdeas} className="generate-ideas-button">
+          Generate 3 ideas
+        </button>
+        {ideasToRender &&
+          ideasToRender.map((idea) => (
+            <section key={idea.id}>
+              <h4>{idea.text}</h4>
+              <p>------------</p>
+            </section>
+          ))}
       </article>
     </>
   );
