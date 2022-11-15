8a3c6b98c9d9e814e0886a64194af00077140959
Korneliu <olbiton@gmail.com>
2022-11-14 18:04:19 -0800

    removed second startgamebutton

--- src/pages/Games/Games.js
+++ src/pages/Games/Games.js
@@ -1,13 +1,37 @@
 import Navbar from "../../components/Navbar/Navbar";
-import { useState, useEffect } from "react";
+import { useState } from "react";
 
 const Games = () => {
-  const [startGame,setStartGameButton] = useState('Click to Start Game')
-  const handleStartNewGame = ()=> {
-    setStartGameButton('Choose')
+  const [startGameButton, setStartGameButton] = useState("start-game-button");
+  const [rpsContainer, setRpsContainer] = useState("hidden")
+  const [result, setResult] = useState("")
+  const [resultArticle, setResultArticle] = useState("hidden")
+  const [startNewGameButton, setStartNewGameButton] = useState("hidden")
+
+  const handleStartNewGame = (e) => {
+    setResult("");
+    setRpsContainer("rps-choice-section-container")
+    setResultArticle("hidden")
+    setStartGameButton("hidden")
   }
+
+
+
   const handleChoiceClick = (e) => {
-    console.log(e.target.innerText)
+    let computerChoice = Math.floor(Math.random() * 3);
+    let userChoice = e.target.innerText
+    //setStartNewGameButton("start-game-button")
+    //setRpsContainer("hidden")
+
+    if(computerChoice === 0 && userChoice === "Rock") {
+      setResultArticle("result-article")
+      setResult("EVEN")
+      setStartGameButton("start-game-button")
+    } else if (computerChoice === 0 && userChoice === "Paper") {
+
+    } else if (computerChoice === 1 && userChoice === "Scissors") {
+
+    }
   }
   return (
     <article>
@@ -17,22 +41,23 @@ const Games = () => {
         <h3>Win/loose ratio: 3/6</h3>
         <article className="rps-container">
           <section className="players">USER</section>
-          <button className="start-game-button" onClick={handleStartNewGame}>
-            {startGame}
+          <button className={startGameButton} onClick={handleStartNewGame}>
+            Click to start new game!
           </button>
           <section className="players">COMP</section>
         </article>
-        <article className="rps-choice-section-container">
+        <article className={rpsContainer}>
           <section onClick={handleChoiceClick} className="choice-section">
             Rock
           </section>
           <section onClick={handleChoiceClick} className="choice-section">
             Paper
           </section>
-          <section onClick={handleChoiceClick}  className="choice-section">
+          <section onClick={handleChoiceClick} className="choice-section">
             Scissors
           </section>
         </article>
+        <article className={resultArticle}>{result}</article>
       </article>
     </article>
   );
