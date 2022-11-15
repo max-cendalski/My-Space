fb7d213e9da43cbd333bf4ea6498bb1fa11c26de
Korneliu <olbiton@gmail.com>
2022-11-14 16:21:45 -0800

    added choice section

--- src/pages/Games/Games.js
+++ src/pages/Games/Games.js
@@ -2,9 +2,9 @@ import Navbar from "../../components/Navbar/Navbar";
 import { useState, useEffect } from "react";
 
 const Games = () => {
-
+  const [startGame,setStartGameButton] = useState('Click to Start Game')
   const handleStartNewGame = ()=> {
-    console.log('whee')
+    setStartGameButton('Choose')
   }
   return (
     <article>
@@ -13,9 +13,9 @@ const Games = () => {
         <h1>Rock Paper Scissors</h1>
         <h3>Win/loose ratio: 3/6</h3>
         <article className="rps-container">
-          <section className="user-section">USER</section>
-          <button className="start-game-button" onClick={handleStartNewGame}>Click to Start Game</button>
-          <section className="computer-section">COMP</section>
+          <section className="players">USER</section>
+          <button className="start-game-button" onClick={handleStartNewGame}>{startGame}</button>
+          <section className="players">COMP</section>
         </article>
       </article>
     </article>
