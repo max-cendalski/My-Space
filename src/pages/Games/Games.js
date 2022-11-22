db7e4fd18fe7cf4c3749a71dd5df4a470861d52a
Korneliu <olbiton@gmail.com>
2022-11-21 16:06:22 -0800

    changed useState for icons, scores

--- src/pages/Games/Games.js
+++ src/pages/Games/Games.js
@@ -11,10 +11,14 @@ const Games = () => {
   const [rpsContainer, setRpsContainer] = useState("hidden");
   const [result, setResult] = useState("");
   const [resultArticle, setResultArticle] = useState("hidden");
-  const [userScore, setUserScore] = useState(0);
-  const [computerScore, setComputerScore] = useState(0);
-  const [userIcon, setUserIcon] = useState("");
-  const [computerIcon, setComputerIcon] = useState("");
+  const [icons, setIcons] = useState({
+    userIcon: "",
+    computerIcon: "",
+  });
+  const [scores, setScores] = useState({
+    userScore: 0,
+    computerScore: 0,
+  });
 
   useEffect(() => {
     const getData = async () => {
@@ -22,8 +26,10 @@ const Games = () => {
         const dataFromFirebase = await getDoc(
           doc(db, "users", user.uid, "games", "rps")
         );
-        setUserScore(dataFromFirebase.data().user);
-        setComputerScore(dataFromFirebase.data().computer);
+        setScores({
+          userScore: dataFromFirebase.data().user,
+          computerScore: dataFromFirebase.data().computer
+        });
       } catch (err) {
         console.error("ERROR: ", err);
       }
@@ -40,8 +46,8 @@ const Games = () => {
     const addGameData = async () => {
       try {
         await setDoc(doc(db, "users", user.uid, "games", "rps"), {
-          user: userScore,
-          computer: computerScore,
+          user: scores.userScore,
+          computer: scores.computerScore,
         });
       } catch (err) {
         console.error("ERROR: ", err);
@@ -62,16 +68,23 @@ const Games = () => {
     } else if (e.target.className === "fa-solid fa-scissors fa-2xl") {
       userChoice = 2;
     }
+
     if (userChoice === computerChoice) {
       if (userChoice === 0) {
-        setUserIcon("fa-solid fa-gem");
-        setComputerIcon("fa-solid fa-gem");
+        setIcons({
+          userIcon: "fa-solid fa-gem",
+          computerIcon: "fa-solid fa-gem",
+        });
       } else if (userChoice === 1) {
-        setUserIcon("fa-regular fa-map");
-        setComputerIcon("fa-regular fa-map");
+        setIcons({
+          userIcon: "fa-solid fa-map",
+          computerIcon: "fa-solid fa-map",
+        });
       } else {
-        setUserIcon("fa-solid fa-scissors");
-        setComputerIcon("fa-solid fa-scissors");
+        setIcons({
+          userIcon: "fa-solid fa-scissors",
+          computerIcon: "fa-solid fa-scissors",
+        });
       }
       setResultArticle("result-article");
       setResult("EVEN!");
@@ -81,51 +94,86 @@ const Games = () => {
       if (userChoice === 0 && computerChoice === 1) {
         setResultArticle("result-article");
         setResult("COMPUTER WON!");
-        setUserIcon("fa-solid fa-gem");
-        setComputerIcon("fa-regular fa-map ");
+
         setRpsContainer("hidden");
         setStartGameButton("start-game-button");
-        setComputerScore(computerScore + 1);
+        setIcons({
+          userIcon: "fa-solid fa-gem",
+          computerIcon: "fa-solid fa-map",
+        });
+        setScores({
+          userScore:scores.userScore,
+          computerScore: scores.computerScore + 1,
+        });
       } else if (userChoice === 1 && computerChoice === 2) {
         setResultArticle("result-article");
         setResult("COMPUTER WON!");
-        setComputerIcon("fa-solid fa-scissors");
-        setUserIcon("fa-regular fa-map ");
         setRpsContainer("hidden");
         setStartGameButton("start-game-button");
-        setComputerScore(computerScore + 1);
+        setScores({
+          userScore: scores.userScore,
+          computerScore: scores.computerScore + 1,
+        });
+         setIcons({
+           userIcon: "fa-solid fa-map",
+           computerIcon: "fa-solid fa-scissors",
+         });
       } else if (userChoice === 2 && computerChoice === 0) {
         setResultArticle("result-article");
         setResult("COMPUTER WON!");
-        setComputerIcon("fa-solid fa-gem");
-        setUserIcon("fa-solid fa-scissors");
+
         setRpsContainer("hidden");
         setStartGameButton("start-game-button");
-        setComputerScore(computerScore + 1);
+        setScores({
+          userScore: scores.userScore,
+          computerScore: scores.computerScore + 1,
+        });
+         setIcons({
+          userIcon: "fa-solid fa-scissors",
+          computerIcon: "fa-solid fa-gem",
+        });
       } else if (userChoice === 0 && computerChoice === 2) {
         setResultArticle("result-article");
         setResult("YOU WON!");
-        setUserIcon("fa-solid fa-gem");
-        setComputerIcon("fa-solid fa-scissors ");
+
         setRpsContainer("hidden");
         setStartGameButton("start-game-button");
-        setUserScore(userScore + 1);
+        setScores({
+          userScore: scores.userScore + 1,
+          computerScore: scores.computerScore
+        });
+         setIcons({
+          userIcon: "fa-solid fa-gem",
+          computerIcon: "fa-solid fa-scissors",
+        });
       } else if (userChoice === 1 && computerChoice === 0) {
         setResultArticle("result-article");
         setResult("YOU WON!");
-        setUserIcon("fa-regular fa-map");
-        setComputerIcon("fa-solid fa-gem");
+
         setRpsContainer("hidden");
         setStartGameButton("start-game-button");
-        setUserScore(userScore + 1);
+        setScores({
+          userScore: scores.userScore + 1,
+          computerScore: scores.computerScore
+        });
+         setIcons({
+          userIcon: "fa-solid fa-map",
+          computerIcon: "fa-solid fa-gem",
+        });
       } else if (userChoice === 2 && computerChoice === 1) {
         setResultArticle("result-article");
         setResult("YOU WON!");
-        setUserIcon("fa-solid fa-scissors");
-        setComputerIcon("fa-regular fa-map");
+
         setRpsContainer("hidden");
         setStartGameButton("start-game-button");
-        setUserScore(userScore + 1);
+        setScores({
+          userScore: scores.userScore + 1,
+          computerScore: scores.computerScore
+        });
+         setIcons({
+          userIcon: "fa-solid fa-scissors",
+          computerIcon: "fa-solid fa-map",
+        });
       }
     }
   };
@@ -139,7 +187,7 @@ const Games = () => {
           <h1 className="header">Rock Paper Scissors</h1>
         </header>
         <h2 className="rps-ratio">
-          Win/loose ratio: {userScore} / {computerScore}
+          Win/loose ratio: {scores.userScore} / {scores.computerScore}
         </h2>
         <article className="rps-container">
           <section className="players">USER</section>
@@ -160,10 +208,10 @@ const Games = () => {
         </article>
         <article className={resultArticle}>
           <section>
-            <i className={userIcon}></i>
+            <i className={icons.userIcon}></i>
           </section>
           <section>
-            <i className={computerIcon}></i>
+            <i className={icons.computerIcon}></i>
           </section>
         </article>
         <h1 className="game-result-header">{result}</h1>
