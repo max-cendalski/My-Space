e1dbe4cf75fbce064f3b8a82b56610ba1d99144e
Korneliu <olbiton@gmail.com>
2022-11-22 19:03:47 -0800

    added first fetch call

--- src/pages/Weather/Weather.js
+++ src/pages/Weather/Weather.js
@@ -1,16 +1,25 @@
-import Navbar from "../../components/Navbar/Navbar"
+import Navbar from "../../components/Navbar/Navbar";
 import GoBack from "../../components/GoBack/GoBack";
 import { useState, useEffect } from "react";
 
 const Weather = () => {
-  const weatherApiKey = process.env.REACT_APP_APP_WEATHER_API_KEY;
-  const getWeather = async()=>{
-    const weather = await fetch(
-      'https://api.openweathermap.org/data/3.0/onecall?lat=33.44&lon=-94.04&exclude=hourly,daily&appid=738338f10d8524c57d795ce279f2a0a7'
-    );
-      console.log("weahter", weather);
-  }
-  getWeather()
+  const [temperature, setTemperature] = useState(0)
+  useEffect(() => {
+    const fetchtWeather = async () => {
+      const weatherApiKey = process.env.REACT_APP_WEATHER_API_KEY;
+      const API_URL = `https://api.openweathermap.org/data/3.0/onecall?lat=33.44&lon=-94.04&units=imperial&appid=${weatherApiKey}`;
+      try {
+        const response = await fetch(API_URL);
+        const data = await response.json();
+        console.log("data", data.current.temp);
+        setTemperature(data.current.temp)
+      } catch (err) {
+        console.error("ERROR: ", err);
+      }
+    };
+
+    fetchtWeather();
+  },[]);
 
   return (
     <article>
@@ -18,10 +27,10 @@ const Weather = () => {
       <article id="weather-page-container">
         <GoBack />
         <h1>Weather</h1>
-
+        <p>In Chicago there is : {temperature} degree fahrenheit</p>
       </article>
     </article>
   );
-}
+};
 
-export default Weather
+export default Weather;
