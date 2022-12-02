a6c2053decdd974de50c8b9bdd0af3ee7e801362
Korneliu <olbiton@gmail.com>
2022-12-01 18:16:20 -0800

    added states for city and temperature

--- src/pages/Weather/Weather.js
+++ src/pages/Weather/Weather.js
@@ -6,34 +6,39 @@ import { geocodeByAddress, getLatLng } from "react-places-autocomplete";
 import { useState, useEffect } from "react";
 
 const Weather = () => {
-  const [temperature, setTemperature] = useState(0);
-  //const [location, setLocation] = useState("");
+  const [temperature, setTemperature] = useState(null);
   const [address, setAddress] = useState("");
   const [latLng, setLatLng] = useState(null);
+  const [city, setCity] = useState("")
 
   useEffect(() => {
-    const fetchtWeather = async () => {
-      const weatherApiKey = process.env.REACT_APP_WEATHER_API_KEY;
-      const API_URL = `https://api.openweathermap.org/data/3.0/onecall?lat=33.44&lon=-94.04&units=imperial&appid=${weatherApiKey}`;
-      try {
-        const response = await fetch(API_URL);
-        const data = await response.json();
-        console.log("data", data.current.temp);
-        setTemperature(data.current.temp);
-      } catch (err) {
-        console.error("ERROR: ", err.message);
-      }
-    };
+    console.log("latLng", latLng);
 
-    fetchtWeather();
-  }, []);
+    if (latLng !== null) {
+      const fetchtWeather = async () => {
+        const weatherApiKey = process.env.REACT_APP_WEATHER_API_KEY;
+        const API_URL = `https://api.openweathermap.org/data/3.0/onecall?lat=${latLng.lat}&lon=${latLng.lng}&units=imperial&appid=${weatherApiKey}`;
+        try {
+          const response = await fetch(API_URL);
+          const data = await response.json();
+          console.log("data", data.current.temp);
+          setTemperature(data.current.temp);
+        } catch (err) {
+          console.error("ERROR: ", err.message);
+        }
+      };
 
-  const handleChange = address => {
-    console.log("address", address);
+      fetchtWeather();
+    }
+  }, [latLng]);
+
+  const handleChange = (address) => {
     setAddress(address);
+    const cityToSave = address.split(",")
+    setCity(cityToSave[0])
   };
 
-  const handleSelect = address => {
+  const handleSelect = (address) => {
     geocodeByAddress(address)
       .then((results) => getLatLng(results[0]))
       .then((latLng) => setLatLng(latLng))
@@ -42,7 +47,8 @@ const Weather = () => {
   };
 
 
-  console.log('latLng',latLng)
+  console.log("address", address);
+  console.log("city", city);
 
   return (
     <article>
@@ -56,6 +62,7 @@ const Weather = () => {
           handleSelect={handleSelect}
         />
       </article>
+      {temperature && <h3> Temperature in {city} is : {temperature}</h3>}
     </article>
   );
 };
