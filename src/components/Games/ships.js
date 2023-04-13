401cb03d5c10364550fd1cb054daaac7fb6ba79e
Korneliu <olbiton@gmail.com>
2023-04-12 21:21:21 -0700

    added game-area

--- src/components/Games/ships.js
+++ src/components/Games/ships.js
@@ -5,7 +5,7 @@ import GoBack from "../GoBack/GoBack";
 const ShipsGame = () => {
   const [squares, setSquares] = useState([]);
   const [alphabet, setAlphabet] = useState([]);
-  const [yAxisMobile, setYMobile] = useState([0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10,11,12,13]);
+  const [yAxisMobile, setYMobile] = useState([0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10,11,12,13,14,15,16,17,18,19,20]);
 
   useEffect(() => {
     const alphabet = [
@@ -37,7 +37,7 @@ const ShipsGame = () => {
       "z", */
     ];
     let arr = [];
-    for (var i = 0; i <= 25; i++) {
+    for (var i = 0; i <= 20; i++) {
       for (var j = 0; j < 15; j++) {
         arr.push({ char: alphabet[j], num: i });
       }
