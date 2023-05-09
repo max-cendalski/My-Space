2f237a82f12b570d7aa1279f4f51640a0fa6484e
Korneliu <olbiton@gmail.com>
2023-05-08 21:19:01 -0700

    added conditional statement to render different class on id

--- src/components/Notes/NotesList.js
+++ src/components/Notes/NotesList.js
@@ -4,11 +4,18 @@ const NotesList = ({ isVisible,notes, deleteNote }) => {
   return (
     <article id="notes-page-container">
       {notes.map((note) => (
-        <article className="single-note-container-tilt-left" key={note.id}>
+        <section
+          className={
+            isNaN(note.id.charAt(0))
+              ? "single-note-container-tilt-left"
+              : "single-note-container-tilt-right"
+          }
+          key={note.id}
+        >
           <section>
             <h5 className="notes-header-list">{note.title}</h5>
           </section>
-        </article>
+        </section>
       ))}
     </article>
   );
