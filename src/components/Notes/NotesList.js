5270618cd11b12e56ea3ed9435f63a31e8b94b91
Korneliu <olbiton@gmail.com>
2023-05-08 21:10:03 -0700

    added tilted notes

--- src/components/Notes/NotesList.js
+++ src/components/Notes/NotesList.js
@@ -4,11 +4,9 @@ const NotesList = ({ isVisible,notes, deleteNote }) => {
   return (
     <article id="notes-page-container">
       {notes.map((note) => (
-        <article className="single-note-container" key={note.id}>
+        <article className="single-note-container-tilt-left" key={note.id}>
           <section>
-            <p>{note.title}</p>
-
-
+            <h5 className="notes-header-list">{note.title}</h5>
           </section>
         </article>
       ))}
@@ -24,12 +22,14 @@ export default NotesList;
 
 
     // <footer className="single-note-footer">
-    //   <button className="delete-button" onClick={() => deleteNote(note.id)}>
-    //     Delete
-    //   </button>
-    // </footer>;
+    //           <button
+    //             className="delete-button"
+    //             onClick={() => deleteNote(note.id)}
+    //           >
+    //             Delete
+    //           </button>
+    //         </footer>
 
-
-      // <Link className="edit-link" to={`/notes/edit/${note.id}`}>
-      //   <p>{note.text}</p>
-      // </Link>;
+/*        <Link className="edit-link" to={`/notes/edit/${note.id}`}>
+         <p>{note.text}</p>
+       </Link>; */
