25632503615b92be9a9333b38ea3096999f3d7af
Korneliu <olbiton@gmail.com>
2023-05-09 18:00:57 -0700

    styled main notes container

--- src/components/Notes/NotesList.js
+++ src/components/Notes/NotesList.js
@@ -1,9 +1,9 @@
+import { useState } from "react";
 import { Link } from "react-router-dom";
-import PinL from "./../../PinL.png"
+import PinL from "./../../PinL.png";
 
-
-
-const NotesList = ({ isVisible,notes, deleteNote }) => {
+const NotesList = ({ isVisible, notes, deleteNote }) => {
+  const [selectedNote, setSelectedNote] = useState(null);
   return (
     <article id="notes-page-container">
       {notes.map((note) => (
@@ -14,33 +14,43 @@ const NotesList = ({ isVisible,notes, deleteNote }) => {
               : "single-note-container-tilt-right"
           }`}
           key={note.id}
+          onClick={() => {
+            console.log("note", note);
+            setSelectedNote(note);
+          }}
         >
           <section>
-            <img className="notes-pin" alt="pin" src={PinL} height="20px"/>
-            <h5 className="notes-header-list">{note.title.substr(0,12)}...</h5>
-            <p className="notes-text-tiny">{note.text.substr(0,80)}...</p>
+            <img className="notes-pin" alt="pin" src={PinL} height="20px" />
+            <h5 className="notes-header-list">{note.title.substr(0, 12)}...</h5>
+            <p className="notes-text-tiny">{note.text.substr(0, 80)}...</p>
           </section>
         </section>
       ))}
+      {selectedNote && (
+        <section className="selected">
+          <img className="notes-pin" alt="pin" src={PinL} height="20px" />
+          <h2>{selectedNote.title}</h2>
+          <p>{selectedNote.text}</p>
+          <p>{selectedNote.date}</p>
+          <button onClick={() => setSelectedNote(null)}>Close</button>
+        </section>
+      )}
     </article>
   );
 };
 
 export default NotesList;
 
-
-
 //<p className="date-paragraph">Created: {note.date}</p>;
 
-
-    // <footer className="single-note-footer">
-    //           <button
-    //             className="delete-button"
-    //             onClick={() => deleteNote(note.id)}
-    //           >
-    //             Delete
-    //           </button>
-    //         </footer>
+// <footer className="single-note-footer">
+//           <button
+//             className="delete-button"
+//             onClick={() => deleteNote(note.id)}
+//           >
+//             Delete
+//           </button>
+//         </footer>
 
 /*        <Link className="edit-link" to={`/notes/edit/${note.id}`}>
          <p>{note.text}</p>
