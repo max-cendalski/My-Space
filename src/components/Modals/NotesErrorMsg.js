c2ed813a6ae304d3c1bd3783c8bc6ff37a5d0bcc
Korneliu <olbiton@gmail.com>
2023-05-19 20:27:50 -0700

    remove unnecessary code

---
+++ src/components/Modals/NotesErrorMsg.js
@@ -0,0 +1,13 @@
+import { useState } from "react"
+
+const NotesErrorMsg = () => {
+
+  return (
+   <article className="notes-error-msg-container">
+    <p>Title and Text can't be empty</p>
+   </article>
+  )
+
+}
+
+export default NotesErrorMsg
--- src/components/Notes/NotesList.js
+++ src/components/Notes/NotesList.js
@@ -4,11 +4,10 @@ import PinL from "../../icons/PinL.png";
 import TrashL from "../../icons/trashL.png";
 import Close from "../../icons/closeS.png";
 import Pencil from "../../icons/pencilS.png";
-import NoteEdit from "../../pages/Notes/EditNote";
 
 const NotesList = ({ isVisible, notes, deleteNote }) => {
   const [selectedNote, setSelectedNote] = useState(null);
-  const [showEditWindow, setShowEditWindow] = useState(false)
+
   const handleCloseButton = (e) => {
     e.stopPropagation();
     setSelectedNote(null);
@@ -52,14 +51,8 @@ const NotesList = ({ isVisible, notes, deleteNote }) => {
               <p className="notes-text-small">{note.text}</p>
               <p className="date-paragraph">Created: {note.date}</p>
               <footer className="note-footer">
-
-
                 <Link className="edit-link" to={`/notes/edit/${note.id}`}>
-                  <img
-                    className="note-pencil"
-                    src={Pencil}
-                    alt="pencil-icon"
-                  />
+                  <img className="note-pencil" src={Pencil} alt="pencil-icon" />
                 </Link>
                 <img
                   className="note-trash"
@@ -72,49 +65,8 @@ const NotesList = ({ isVisible, notes, deleteNote }) => {
           )}
         </section>
       ))}
-      {showEditWindow && <NoteEdit />}
     </article>
   );
 };
 
 export default NotesList;
-
-//<p className="date-paragraph">Created: {note.date}</p>;
-
-// <footer className="single-note-footer">
-//           <button
-//             className="delete-button"
-//             onClick={() => deleteNote(note.id)}
-//           >
-//             Delete
-//           </button>
-//         </footer>
-
-/*        <Link className="edit-link" to={`/notes/edit/${note.id}`}>
-         <p>{note.text}</p>
-       </Link>; */
-
-//  {selectedNote && note.id === selectedNote.id ? (
-//     ""
-//   ) : (
-//     <img className="notes-pin" alt="pin" src={PinL} height="20px" />
-//   )}
-
-//   <h4 className="note-header-list">
-//     {" "}
-
-//     {selectedNote && note.id === selectedNote.id
-//       ? note.title
-//       : note.title.substr(0, 10) + "..."}{" "}
-//   </h4>
-//   <p className="notes-text-small">
-
-//     {selectedNote && note.id === selectedNote.id
-//       ? note.text
-//       : note.text.substr(0, 20) + "..."}{" "}
-//   </p>
-//   {selectedNote && note.id === selectedNote.id ? (
-//     <p className="date-paragraph">Created: {note.date}</p>
-//   ) : (
-//     ""
-//   )}
