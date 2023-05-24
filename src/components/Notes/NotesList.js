8207d0e7b476372a9c4ce6ca3b7ecc3cbb50d9b4
Korneliu <olbiton@gmail.com>
2023-05-23 19:38:07 -0700

    finished notes

--- src/components/Notes/NotesList.js
+++ src/components/Notes/NotesList.js
@@ -15,60 +15,64 @@ const NotesList = ({ isVisible, notes, deleteNote }) => {
     return null;
   }
   const handleSelected = (e) => {
-    e.stopPropagation()
+    e.stopPropagation();
     setSelectedNote(null);
   };
 
   return (
     <>
-    <article className="notes-list-container">
-      {notes.map((note) => (
-        <section
-          className={`single-note-container ${
-            note.toBeRemoved ? `note-to-be-removed` : ""
-          } `}
-          key={note.id}
-          onClick={() => {
-            setSelectedNote(note);
-          }}
-        >
-          <section>
-            <img className="notes-pin" alt="pin" src={PinL} height="20px" />
-            <h4 className="note-header-list">
-              {note.title.substr(0, 10) + "..."}
-            </h4>
-            <p className="notes-text-small">
-              {note.text.substr(0, 20) + "..."}
-            </p>
+      <article className="notes-list-container">
+        {notes.map((note) => (
+          <section
+            className={`single-note-container ${
+              note.toBeRemoved ? `note-to-be-removed` : ""
+            } `}
+            key={note.id}
+            onClick={() => {
+              setSelectedNote(note);
+            }}
+          >
+            <section>
+              <img className="notes-pin" alt="pin" src={PinL} height="20px" />
+              <h4 className="note-header-list">
+                {note.title.substr(0, 10) + "..."}
+              </h4>
+              <p className="notes-text-small">
+                {note.text.substr(0, 20) + "..."}
+              </p>
+            </section>
+              {selectedNote && note.id === selectedNote.id && (
+                <article className="selected-note">
+                  <img
+                    className="note-close"
+                    src={Close}
+                    alt="x"
+                    onClick={handleCloseButton}
+                  />
+                  <h4 className="note-header-list">{note.title}</h4>
+                  <p className="notes-text-small">{note.text}</p>
+                  <p className="date-paragraph">Created: {note.date}</p>
+                  <footer onClick={handleSelected} className="note-footer">
+                    <Link className="edit-link" to={`/notes/edit/${note.id}`}>
+                      <img
+                        className="note-pencil"
+                        src={Pencil}
+                        alt="pencil-icon"
+                      />
+                    </Link>
+                    <img
+                      className="note-trash"
+                      src={TrashL}
+                      alt="trash-icon"
+                      onClick={() => deleteNote(note.id)}
+                    />
+                  </footer>
+                </article>
+              )}
           </section>
-          {selectedNote && note.id === selectedNote.id && (
-            <article className="selected-note">
-              <img
-                className="note-close"
-                src={Close}
-                alt="x"
-                onClick={handleCloseButton}
-              />
-              <h4 className="note-header-list">{note.title}</h4>
-              <p className="notes-text-small">{note.text}</p>
-              <p className="date-paragraph">Created: {note.date}</p>
-              <footer onClick={handleSelected} className="note-footer">
-                <Link className="edit-link" to={`/notes/edit/${note.id}`}>
-                  <img className="note-pencil" src={Pencil} alt="pencil-icon" />
-                </Link>
-                <img
-                  className="note-trash"
-                  src={TrashL}
-                  alt="trash-icon"
-                  onClick={() => deleteNote(note.id)}
-                />
-              </footer>
-            </article>
-          )}
-        </section>
-      ))}
-    </article>
-    <article className={`${selectedNote ? "overlay": ""}`}></article>
+        ))}
+      </article>
+      <article className={`${selectedNote ? "overlay" : "hidden"}`}></article>
     </>
   );
 };
