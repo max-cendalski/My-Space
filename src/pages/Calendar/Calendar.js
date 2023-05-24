fdbc6268c33ad1dbab72a3ed69eaf2f89fb37973
Korneliu <olbiton@gmail.com>
2023-05-23 20:59:45 -0700

    modyfied css

--- src/pages/Calendar/Calendar.js
+++ src/pages/Calendar/Calendar.js
@@ -1,16 +1,17 @@
 import React from "react";
-//import Navbar from "../../components/Navbar/Navbar";
+import Navbar from "../../components/Navbar/Navbar";
 import FullCalendar from "@fullcalendar/react";
 import dayGridPlugin from "@fullcalendar/daygrid";
 
-export default class Calendar extends React.Component {
-  render() {
-    return (
-      <FullCalendar
-        plugins={[dayGridPlugin]}
-        initialView="dayGridMonth"
+const Calendar =() => {
 
-      />
+    return (
+      <>
+        <Navbar />
+        <FullCalendar plugins={[dayGridPlugin]} initialView="dayGridMonth" />
+      </>
     );
-  }
+
 }
+
+export default Calendar;
