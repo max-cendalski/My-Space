1eb8d84cdbc75a85c4f9b4cabd5d4d0eed8d75ce
Korneliu <olbiton@gmail.com>
2023-05-23 20:27:21 -0700

    change Navbar position, added fragments

--- src/pages/Calendar/Calendar.js
+++ src/pages/Calendar/Calendar.js
@@ -1,15 +1,17 @@
-import Navbar from '../../components/Navbar/Navbar';
+import Navbar from "../../components/Navbar/Navbar";
 
 export const Calendar = () => {
-
   return (
-    <article id="calendar-container">
+    <>
       <Navbar />
-      <article className='calendar-header-container'>
-        <h1>Task</h1>
+
+      <article id="calendar-container">
+        <article className="calendar-header-container">
+          <h1>Task</h1>
+        </article>
       </article>
-    </article>
+    </>
   );
-}
+};
 
 export default Calendar;
