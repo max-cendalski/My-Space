c612ab2aee5797af5d248f3714c02c277c650a75
Korneliu <olbiton@gmail.com>
2023-06-05 22:31:52 -0700

    added handleClickEvent

--- src/components/Calendar/fullcalendar.js
+++ src/components/Calendar/fullcalendar.js
@@ -1,14 +1,15 @@
 import FullCalendar from "@fullcalendar/react";
 import dayGridPlugin from "@fullcalendar/daygrid";
+import interactionPlugin from "@fullcalendar/interaction"
 
 
 const CalendarComponent = () => {
-  const handleDateClick = (arg) => { // bind with an arrow function
+  const handleDateClick = (arg) => {
     alert(arg.dateStr);
   }
   return (
     <FullCalendar
-      plugins={[dayGridPlugin]}
+      plugins={[dayGridPlugin, interactionPlugin]}
       initialView="dayGridMonth"
       headerToolbar={{
         left: "prev,next today",
