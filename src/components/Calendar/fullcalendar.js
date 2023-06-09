2cc18cbe2239d4c7b7cebdef921bbe7e1737eb25
Korneliu <olbiton@gmail.com>
2023-06-08 19:30:10 -0700

    changed button colors in calendar

--- src/components/Calendar/fullcalendar.js
+++ src/components/Calendar/fullcalendar.js
@@ -5,7 +5,6 @@ import FullCalendar from '@fullcalendar/react';
 import dayGridPlugin from '@fullcalendar/daygrid';
 import timeGridPlugin from '@fullcalendar/timegrid';
 import interactionPlugin from '@fullcalendar/interaction';
-//import {  createEventId } from './event-utils';
 import { UserAuth } from "../../context/AuthContext";
 
 function CalendarComponent() {
@@ -15,7 +14,6 @@ function CalendarComponent() {
   const calendarRef = React.useRef(null);
 
   
-
   useEffect(() => {
     const unsub = onSnapshot(
       collection(db, `users/${user.uid}`, "calendarEvents"),
@@ -116,7 +114,6 @@ function CalendarComponent() {
   }
 
   const handleEvents = (events) => {
-    console.log('eventsss',events)
     setCurrentEvents(events);
   }
 
@@ -129,6 +126,7 @@ function CalendarComponent() {
       <div className='demo-app-main'>
         <FullCalendar
         ref={calendarRef}
+        longPressDelay={500} // sets the delay for press and hold
           plugins={[dayGridPlugin, timeGridPlugin, interactionPlugin]}
           headerToolbar={{
             left: 'prev,next today',
@@ -188,124 +186,4 @@ export default CalendarComponent;
 
 
 
-
-
-
-
-
-// import React from 'react'
-// import { formatDate } from '@fullcalendar/core'
-// import FullCalendar from '@fullcalendar/react'
-// import dayGridPlugin from '@fullcalendar/daygrid'
-// import timeGridPlugin from '@fullcalendar/timegrid'
-// import interactionPlugin from '@fullcalendar/interaction'
-// import { INITIAL_EVENTS, createEventId } from './event-utils'
-
-// export default class CalendarComponent extends React.Component {
-
-//   state = {
-//     weekendsVisible: true,
-//     currentEvents: []
-//   }
-
-//   render() {
-//     return (
-     
-//         <article className='calendar-container'>
-//           {this.renderSidebar()}
-//           <div className='demo-app-main'>
-//             <FullCalendar
-//               plugins={[dayGridPlugin, timeGridPlugin, interactionPlugin]}
-//               headerToolbar={{
-//                 left: 'prev,next today',
-//                 center: 'title',
-//                 right: 'dayGridMonth,timeGridDay'
-//               }}
-//               initialView='dayGridMonth'
-//               editable={true}
-//               selectable={true}
-//               selectMirror={true}
-//               dayMaxEvents={true}
-//               weekends={this.state.weekendsVisible}
-//               initialEvents={INITIAL_EVENTS} // alternatively, use the `events` setting to fetch from a feed
-//               select={this.handleDateSelect}
-//               eventContent={renderEventContent} // custom render function
-//               eventClick={this.handleEventClick}
-//               eventsSet={this.handleEvents} // called after events are initialized/added/changed/removed
-//             /* you can update a remote database when these fire:
-//             eventAdd={function(){}}
-//             eventChange={function(){}}
-//             eventRemove={function(){}}
-//             */
-//             />
-//           </div>
-//         </article>
-    
-//     )
-//   }
-
-//   renderSidebar() {
-//     return (
-//       <div className='demo-app-sidebar'>
-
-//         <div className='demo-app-sidebar-section'>
-//           <label>
-//             <input
-//               type='checkbox'
-//               checked={this.state.weekendsVisible}
-//               onChange={this.handleWeekendsToggle}
-//             ></input>
-//             toggle weekends
-//           </label>
-//         </div>
-
-//       </div>
-//     )
-//   }
-
-//   handleWeekendsToggle = () => {
-//     this.setState({
-//       weekendsVisible: !this.state.weekendsVisible
-//     })
-//   }
-
-//   handleDateSelect = (selectInfo) => {
-//     let title = prompt('Please enter a new title for your event')
-//     let calendarApi = selectInfo.view.calendar
-
-//     calendarApi.unselect() // clear date selection
-
-//     if (title) {
-//       calendarApi.addEvent({
-//         id: createEventId(),
-//         title,
-//         start: selectInfo.startStr,
-//         end: selectInfo.endStr,
-//         allDay: selectInfo.allDay
-//       })
-//     }
-//   }
-
-//   handleEventClick = (clickInfo) => {
-//     if (window.confirm(`Are you sure you want to delete the event '${clickInfo.event.title}'`)) {
-//       clickInfo.event.remove()
-//     }
-//   }
-
-//   handleEvents = (events) => {
-//     this.setState({
-//       currentEvents: events
-//     })
-//   }
-
-// }
-
-// function renderEventContent(eventInfo) {
-//   return (
-//     <>
-//       <b>{eventInfo.timeText}</b>
-//       <i>{eventInfo.event.title}</i>
-//     </>
-//   )
-// }
 
