ff778f4b4b040a970ea887974f0946d00032c586
Korneliu <olbiton@gmail.com>
2023-06-26 19:59:57 -0700

    created function checkGameProgress

--- src/components/Games/TTT/Ttt.js
+++ src/components/Games/TTT/Ttt.js
@@ -5,7 +5,7 @@ import { useState, useEffect } from "react";
 
 
 export default function TttComponent() {
-    const [squares, setSquares] = useState(Array.from({ length: 9 }, (_, i) => ({
+    const [game, setSquares] = useState(Array.from({ length: 9 }, (_, i) => ({
         index: i,
         clicked: false,
         value: ""
@@ -14,36 +14,57 @@ export default function TttComponent() {
         sign: "X",
         gameMode: "easy"
     })
+    const isGameStarted = game.some(item => item.value !== "");
 
 
-    useEffect(()=> {
-        console.log('userSettings',userSettings)
-    },[userSettings])
+    const checkPosition = (positions) => {
+        return positions.every((index) => game[index].value === userSettings.sign)
+    }
+
+    const checkGameProgress = () => {
+        var player = userSettings.sign
+        const winningPositions = [
+            [0, 1, 2],
+            [3, 4, 5],
+            [6, 7, 8],
+            [0, 3, 6],
+            [1, 4, 7],
+            [2, 5, 8],
+            [0, 4, 8],
+            [2, 4, 6]
+        ];
+
+        for (let position of winningPositions) {
+            if (checkPosition(position, player)) {
+                console.log(`Found X sequence at indices ${position}`);
+                break;
+            }
+        }
+    }
+
+
 
     const handleSquareClick = (index) => {
         if (userSettings.sign === "") return;
 
-        var newSquares = squares.slice();
-        newSquares[index].value = userSettings.sign
+        var newGame = game.slice();
+        newGame[index].value = userSettings.sign
 
-        setSquares(newSquares);
-
+        setSquares(newGame);
+        checkGameProgress()
         setTimeout(() => {
-            var squaresNotClicked = squares.filter(item => item.value === "");
+            var squaresNotClicked = game.filter(item => item.value === "");
 
             if (squaresNotClicked.length > 0) {
                 var squareTochange = squaresNotClicked[Math.floor(Math.random() * squaresNotClicked.length)].index;
-                newSquares = squares.slice();
-                newSquares[squareTochange].value = userSettings.sign === "X" ? "O" : "X";
-                setSquares(newSquares);
+                newGame = game.slice();
+                newGame[squareTochange].value = userSettings.sign === "X" ? "O" : "X";
+                setSquares(newGame);
             }
         }, 800);
     }
 
     const handleUserSettingsChange = ({ target }) => {
-        let valu = squares.some(item => item.value !=="")
-        console.log('value',valu)
-     
         setUserSettings((prevState) => ({
             ...prevState, [target.name]: target.value
         }))
@@ -62,17 +83,17 @@ export default function TttComponent() {
                     setSquares(newSquares)
                 }}>RESET</button>
                 <section className="sign-lvl-choose-section">
-                    <select className="ttt-select-container" value={userSettings.sign} name="sign" onChange={handleUserSettingsChange} disabled={ squares.some(item => item.value !=="")}>
+                    <select className="ttt-select-container" value={userSettings.sign} name="sign" onChange={handleUserSettingsChange} disabled={isGameStarted}>
                         <option value="X">Your sign:  X</option>
                         <option value="O">Your sign:  O</option>
                     </select>
-                    <select className="ttt-select-container" value={userSettings.gameMode} name="gameMode" onChange={handleUserSettingsChange} disabled={ squares.some(item => item.value !=="")}>
+                    <select className="ttt-select-container" value={userSettings.gameMode} name="gameMode" onChange={handleUserSettingsChange} disabled={isGameStarted}>
                         <option value="easy">Difficult Level: Easy</option>
                         <option value="hard">Difficult Level: Hard</option>
                     </select>
                 </section>
                 <article className="ttt-game-area-container">
-                    {squares.map(item => (
+                    {game.map(item => (
                         <Square
                             key={item.index}
                             value={item.value}
