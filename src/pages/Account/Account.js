89454dc5e7f655f00e9f302d069b42b74ed0badf
Korneliu <olbiton@gmail.com>
2023-07-10 16:45:15 -0700

    added isLoading state to account

--- src/pages/Account/Account.js
+++ src/pages/Account/Account.js
@@ -1,5 +1,5 @@
 import { useState, useEffect } from "react"
-import { collection, getDocs,getDoc, doc } from "firebase/firestore";
+import { collection, getDocs, getDoc, doc } from "firebase/firestore";
 import { db } from "../../firebase/Firebase";
 import { UserAuth } from "../../context/AuthContext";
 import Navbar from "../../components/Navbar/Navbar"
@@ -13,6 +13,7 @@ export default function Account() {
         weather: 0,
         games: 0
     })
+    const [isLoading, setIsLoading] = useState(true)
 
     useEffect(() => {
         async function getCollections() {
@@ -32,48 +33,54 @@ export default function Account() {
                 console.error('error', err)
             }
         }
+
         async function getGameData() {
-            const rpsRef = doc(db,"users",user.uid, "games","rps")
-            const  rpsSnap = await getDoc(rpsRef)
+            const rpsRef = doc(db, "users", user.uid, "games", "rps")
+            const rpsSnap = await getDoc(rpsRef)
             if (rpsSnap.exists()) {
-                console.log(rpsSnap.data())
                 let total = rpsSnap.data().computer + rpsSnap.data().user
-                setUserStats(prevState => ({...prevState, games:total}))
+                setUserStats(prevState => ({ ...prevState, games: total }))
             } else {
                 console.log('No such document!')
-            }   
+            }
         }
-        getCollections()
-        getGameData()
+        Promise.all([getGameData(), getCollections()]).then(() => setIsLoading(false));
     }, [user.uid])
+
+
 
     return (
         <>
-            <Navbar />
-            <article id="account-container">
-
-                <h1>My Account</h1>
-                <section className="account-single-section">
-                    <h2>Notes</h2>
-                    <p>You have penned down <span className="user-stats-number">{userStats.notes}</span> notes.</p>
-                </section>
-                <section className="account-single-section">
-                    <h2>Todos</h2>
-                    <p> There are <span className="user-stats-number">{userStats.todos}</span> task on your Todo list.</p>
-                </section>
-                <section className="account-single-section">
-                    <h2>Calendar</h2>
-                    <p>Your calendar has <span className="user-stats-number">{userStats.calendar}</span> events marked.</p>
-                </section>
-                <section className="account-single-section">
-                    <h2>Weather</h2>
-                    <p>You've set up <span className="user-stats-number">{userStats.weather}</span> locations in the Weather section.</p>
-                </section>
-                <section className="account-single-section">
-                    <h2>Games</h2>
-                    <p>You've played <span className="user-stats-number">{userStats.games}</span> times in the RPS game.</p>
-                </section>
-            </article>
+            {isLoading ?
+                <p>Loading...</p>
+                :
+                <article>
+                    <Navbar />
+                    <article id="account-container">
+                        <h1>My Account</h1>
+                        <section className="account-single-section">
+                            <h2>Notes</h2>
+                            {userStats.notes ? <p>You have penned down <span className="user-stats-number">{userStats.notes}</span> notes.</p> : <p>You don't have any notes</p>}
+                        </section>
+                        <section className="account-single-section">
+                            <h2>Todos</h2>
+                            {userStats.todos ? <p> There are <span className="user-stats-number">{userStats.todos}</span> tasks on your Todo list.</p> : <p>There are no tasks on your Todo list.</p>}
+                        </section>
+                        <section className="account-single-section">
+                            <h2>Calendar</h2>
+                            {userStats.calendar ? <p>Your calendar has <span className="user-stats-number">{userStats.calendar}</span> events marked.</p> : <p>Your calendar doesn't have any events marked</p>}
+                        </section>
+                        <section className="account-single-section">
+                            <h2>Weather</h2>
+                            {userStats.weather ? <p>You've set up <span className="user-stats-number">{userStats.weather}</span> locations in the Weather section.</p> : <p>You haven't set up any locations in the Weather section yet</p>}
+                        </section>
+                        <section className="account-single-section">
+                            <h2>Games</h2>
+                            {userStats.games ? <p>You've played <span className="user-stats-number">{userStats.games}</span> times in the RPS game.</p> : <p>You haven't played the RPS game yet.</p>}
+                        </section>
+                    </article>
+                </article>
+            }
         </>
     )
 }
