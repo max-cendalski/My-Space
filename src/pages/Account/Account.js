555ed8b04aecd61faf124bb8148d8bef0e7d6936
Korneliu <olbiton@gmail.com>
2023-07-12 20:29:15 -0700

    styled delete account dialog window

--- src/pages/Account/Account.js
+++ src/pages/Account/Account.js
@@ -68,54 +68,51 @@ export default function Account() {
     };
     return (
         <>
-            <article>
-                <Navbar />
-                {isLoading && (<p className="loading-notification">Loading...</p>)}
-                {!isLoading && (
-                    <article id="account-container">
-                        <h1>My Account</h1>
-                        <section className="account-single-section">
-                            <h2>Notes</h2>
-                            {userStats.notes ? <p>You have penned down <span className="user-stats-number">{userStats.notes}</span> notes.</p> : <p>You don't have any notes</p>}
-                        </section>
-                        <section className="account-single-section">
-                            <h2>Todos</h2>
-                            {userStats.todos ? <p> There are <span className="user-stats-number">{userStats.todos}</span> tasks on your Todo list.</p> : <p>There are no tasks on your Todo list.</p>}
-                        </section>
-                        <section className="account-single-section">
-                            <h2>Calendar</h2>
-                            {userStats.calendar ? <p>Your calendar has <span className="user-stats-number">{userStats.calendar}</span> events marked.</p> : <p>Your calendar doesn't have any events marked</p>}
-                        </section>
-                        <section className="account-single-section">
-                            <h2>Weather</h2>
-                            {userStats.weather ? <p>You've set up <span className="user-stats-number">{userStats.weather}</span> locations in the Weather section.</p> : <p>You haven't set up any locations in the Weather section yet</p>}
-                        </section>
-                        <section className="account-single-section">
-                            <h2>Games</h2>
-                            {userStats.games ? <p>You've played <span className="user-stats-number">{userStats.games}</span> times in the RPS game.</p> : <p>You haven't played the RPS game yet.</p>}
-                        </section>
-                        <section id="delete-account-section">
-                            <h3>Delete Your Account</h3>
-                            <p>Once you delete your account, you will permanently lose all saved data and this cannot be undone.</p>
-                            <button onClick={handleDeleteClick} className="delete-account-button">Delete Account</button>
-                            {dialogStatus &&
-                                <section id="delete-account-dialog-wrapper">
-                                    <dialog open>
-                                        <h3>Delete Your Account</h3>
-                                        <p>Are you sure you want to delete your account? This process cannot be undone.</p>
+            <Navbar />
+            {isLoading && (<p className="loading-notification">Loading...</p>)}
+            {!isLoading && (
+                <article id="account-container">
+                    <h1>My Account</h1>
+                    <section className="account-single-section">
+                        <h2>Notes</h2>
+                        {userStats.notes ? <p>You have penned down <span className="user-stats-number">{userStats.notes}</span> notes.</p> : <p>You don't have any notes</p>}
+                    </section>
+                    <section className="account-single-section">
+                        <h2>Todos</h2>
+                        {userStats.todos ? <p> There are <span className="user-stats-number">{userStats.todos}</span> tasks on your Todo list.</p> : <p>There are no tasks on your Todo list.</p>}
+                    </section>
+                    <section className="account-single-section">
+                        <h2>Calendar</h2>
+                        {userStats.calendar ? <p>Your calendar has <span className="user-stats-number">{userStats.calendar}</span> events marked.</p> : <p>Your calendar doesn't have any events marked</p>}
+                    </section>
+                    <section className="account-single-section">
+                        <h2>Weather</h2>
+                        {userStats.weather ? <p>You've set up <span className="user-stats-number">{userStats.weather}</span> locations in the Weather section.</p> : <p>You haven't set up any locations in the Weather section yet</p>}
+                    </section>
+                    <section className="account-single-section">
+                        <h2>Games</h2>
+                        {userStats.games ? <p>You've played <span className="user-stats-number">{userStats.games}</span> times in the RPS game.</p> : <p>You haven't played the RPS game yet.</p>}
+                    </section>
+                    <section id="delete-account-section">
+                        <h3>Delete Your Account</h3>
+                        <p>Once you delete your account, you will permanently lose all saved data and this cannot be undone.</p>
+                        <button onClick={handleDeleteClick} className="delete-account-button">Delete Account</button>
+                        {dialogStatus &&
+                            <section id="delete-account-dialog-wrapper">
+                                <dialog open>
+                                    <h3>Delete Your Account</h3>
+                                    <p>Are you sure you want to delete your account? This process cannot be undone.</p>
+                                    <footer className="dialog-delete-event-footer">
                                         <button onClick={handleClose} className="close-dialog-account">Cancel</button>
                                         <button onClick={handleConfirm} className="confirm-delete-account-button">Confirm</button>
-                                    </dialog>
-                                </section>
+                                    </footer>
 
-                            }
-                        </section>
-                    </article>
-
-                )}
-
-            </article>
-
+                                </dialog>
+                            </section>
+                        }
+                    </section>
+                </article>
+            )}
         </>
     )
 }
