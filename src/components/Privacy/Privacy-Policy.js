d1ab7eacb0c0c75f53c1bb5e2ff0cadacce37eb3
Korneliu <olbiton@gmail.com>
2024-02-18 21:12:05 -0800

    clean up

--- src/components/Privacy/Privacy-Policy.js
+++ src/components/Privacy/Privacy-Policy.js
@@ -10,11 +10,13 @@ export default function PrivacyPolicy() {
             <ol>
                 <li>**Purpose:** Myspacemaxcen is solely a demonstration project. It is designed to exhibit my abilities in integrating and implementing various functionalities, including Facebook login authentication, within a web application. This app is not intended for commercial use or for general public usage.
                 </li>
-                <li>**Data Collection and Usage:** As this application is for demonstration purposes only, it does not collect, store, use, or share any personal data from its users. Any interaction with the application is purely for the demonstration of its functionalities and does not involve any actual data processing or storage.</li>
-                <li>**No Third-Party Sharing:** There is no sharing of any data with third parties, as no personal data is collected. This includes adherence to strict privacy standards and ensuring the confidentiality of any demonstration data used within the app.</li>
-                <li>**Contact Information:** For any inquiries or questions regarding this privacy policy or the application, please contact me at max@maxcen.dev.
-                    This privacy policy may be updated or changed as required, with the latest version always available on this page.
-                    Thank you for visiting and exploring my portfolio project.</li>
+                <li>**Data Collection and Usage:** As this application is for demonstration purposes only, it does not collect, store, use, or share any personal data from its users. Any interaction with the application is purely for the demonstration of its functionalities and does not involve any actual data processing or storage.
+                </li>
+                <li>**No Third-Party Sharing:** There is no sharing of any data with third parties, as no personal data is collected. This includes adherence to strict privacy standards and ensuring the confidentiality of any demonstration data used within the app.
+                </li>
+                <li>**Contact Information:** For any inquiries or questions regarding this privacy policy or the application, please contact me at max@maxcen.dev.This privacy policy may be updated or changed as required, with the latest version always available on this page.
+                Thank you for visiting and exploring my portfolio project.
+                </li>
             </ol>
 
         </article>
