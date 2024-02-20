7111ff49515e7cacc0a2f0372fc832eb2103428e
Korneliu <olbiton@gmail.com>
2024-02-19 16:15:49 -0800

    adjust privacy-policy

--- src/components/Privacy/Privacy-Policy.js
+++ src/components/Privacy/Privacy-Policy.js
@@ -7,11 +7,11 @@ export default function PrivacyPolicy() {
                 Welcome to Myspacemaxcen. This application has been developed by Max as a part of my professional portfolio to showcase my technical skills in web and app development. Please note the following key points regarding the use of this application:
             </p>
             <ol>
-                <li>**Purpose:** Myspacemaxcen is solely a demonstration project. It is designed to exhibit my abilities in integrating and implementing various functionalities, including Facebook login authentication, within a web application. This app is not intended for commercial use or for general public usage.
+                <li>**Purpose:** Myspacemaxcen is solely a demonstration project. It is designed to exhibit my abilities in integrating and implementing various functionalities, including Google and GitHub login authentication, within a web application. This app is not intended for commercial use or for general public usage.
                 </li>
-                <li>**Data Collection and Usage:** As this application is for demonstration purposes only, it does not collect, store, use, or share any personal data from its users. Any interaction with the application is purely for the demonstration of its functionalities and does not involve any actual data processing or storage.
+                <li>**Data Collection and Usage:** This application requires user login and thus collects data necessary for authentication and user identification. After logging in, to manage your account, click on the user icon located in the top right corner. This will reveal a dropdown menu with several options. Select 'Account' to navigate to your account page. At the bottom of this page, you will find the 'Delete Account' button, which allows you to permanently delete your account and all data associated with it.
                 </li>
-                <li>**No Third-Party Sharing:** There is no sharing of any data with third parties, as no personal data is collected. This includes adherence to strict privacy standards and ensuring the confidentiality of any demonstration data used within the app.
+                <li>**No Third-Party Sharing:** No data is shared with third parties, as the application does not collect personal data beyond what is necessary for demonstration purposes. This includes adherence to strict privacy standards and ensuring the confidentiality of any demonstration data used within the app.
                 </li>
                 <li>**Contact Information:** For any inquiries or questions regarding this privacy policy or the application, please contact me at max@maxcen.dev.This privacy policy may be updated or changed as required, with the latest version always available on this page.
                     Thank you for visiting and exploring my portfolio project.
